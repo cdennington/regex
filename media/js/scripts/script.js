e6dc0f1417b502d749ca97c85512406a33d59ef6
Charlie Dennnington <charlie.dennington@u-dox.com>
2013-07-29 17:12:31 +0100

    user can now check regex against string which they enter

--- media/js/scripts/script.js
+++ media/js/scripts/script.js
@@ -1,29 +1,40 @@
 $(document).ready(function() {
     
+    // Get data from form
     $('.regex_form').submit(function() {
         var regex = $('.regex_dropdown').val();
         var regex_function_name = regex.replace(/ /g,"_");
         var search_key = $('.search_key').val();
-        var fn = regex_function_name + "('" + search_key + "');";
+        var search_sentence = $('.search_sentence').val();
+        var fn = regex_function_name + "('" + search_key + "', '" + search_sentence +"');";
         $('.answer').append('<h3>'+ regex + ' ' + search_key +'</h3>');
         eval(fn);
         return false;
     });
 
-    function A_string_that_starts_with(search_key) {
-    	$('.answer').append('Add a ^ to the beginning of the string, so the regex will look like "^' + search_key + '"');
+    // Check and calculate regex
+    function At_the_beginning_of(search_key, search_sentence) {
+    	var n=search_sentence.match("^" + search_key + "");
+    	$('.answer').append('<p>Add a ^ to the beginning of the string, E.g. "^' + search_key + '"</p>');
+    	$('.answer').append('<p>' + n + '</p>');
     	$('.answer').fadeIn();
     }
-    function A_string_that_ends_with(search_key) {
-    	$('.answer').append('Add a $ to the end of the string, so the regex will look like "' + search_key + '$"');
+    function At_the_end_of(search_key, search_sentence) {
+    	var n=search_sentence.match(search_key + "$");
+    	$('.answer').append('Add a $ to the end of the string, E.g. "' + search_key + '$"');
+    	$('.answer').append('<p>' + n + '</p>');
     	$('.answer').fadeIn();
     }
-    function A_string_that_starts_and_ends_with(search_key) {
-    	$('.answer').append('Add a ^ to the beginning of the string and $ to the end, so the regex will look like "^' + search_key + '"$');
+    function At_the_begining_and_end_of(search_key, search_sentence) {
+    	var n=search_sentence.match("^" + search_key + "$");
+    	$('.answer').append('Add a ^ to the beginning of the string and $ to the end, E.g. "^' + search_key + '"$');
+    	$('.answer').append('<p>' + n + '</p>');
     	$('.answer').fadeIn();
     }
-    function A_string_that_has_the_text(search_key) {
-    	$('.answer').append('Just put the string in quotes, so the regex will look like "' + search_key + '"');
+    function Anywhere(search_key, search_sentence) {
+    	var n=search_sentence.match(search_key);
+    	$('.answer').append('Just put the string in quotes, E.g. "' + search_key + '"');
+    	$('.answer').append('<p>' + n + '</p>');
     	$('.answer').fadeIn();
     }
 
