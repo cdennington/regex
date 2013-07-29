6fdd7043533002c1fd1ef43369cb86a2fdbdaf9c
Charlie Dennnington <charlie.dennington@u-dox.com>
2013-07-29 17:26:13 +0100

    users now get an answer to there regex

--- media/js/scripts/script.js
+++ media/js/scripts/script.js
@@ -14,28 +14,33 @@ $(document).ready(function() {
 
     // Check and calculate regex
     function At_the_beginning_of(search_key, search_sentence) {
-    	var n=search_sentence.match("^" + search_key + "");
+    	var check_regex = search_sentence.match("^" + search_key + "");
     	$('.answer').append('<p>Add a ^ to the beginning of the string, E.g. "^' + search_key + '"</p>');
-    	$('.answer').append('<p>' + n + '</p>');
-    	$('.answer').fadeIn();
+    	answer_regex(check_regex);
     }
     function At_the_end_of(search_key, search_sentence) {
-    	var n=search_sentence.match(search_key + "$");
+    	var check_regex = search_sentence.match(search_key + "$");
     	$('.answer').append('Add a $ to the end of the string, E.g. "' + search_key + '$"');
-    	$('.answer').append('<p>' + n + '</p>');
-    	$('.answer').fadeIn();
+    	answer_regex(check_regex);
     }
     function At_the_begining_and_end_of(search_key, search_sentence) {
-    	var n=search_sentence.match("^" + search_key + "$");
+    	var check_regex = search_sentence.match("^" + search_key + "$");
     	$('.answer').append('Add a ^ to the beginning of the string and $ to the end, E.g. "^' + search_key + '"$');
-    	$('.answer').append('<p>' + n + '</p>');
-    	$('.answer').fadeIn();
+    	answer_regex(check_regex);
     }
     function Anywhere(search_key, search_sentence) {
-    	var n=search_sentence.match(search_key);
+    	var check_regex = search_sentence.match(search_key);
     	$('.answer').append('Just put the string in quotes, E.g. "' + search_key + '"');
-    	$('.answer').append('<p>' + n + '</p>');
-    	$('.answer').fadeIn();
+    	answer_regex(check_regex);
     }
 
+    // Tell user the answer of regex
+    function answer_regex(answer) {
+    	if (answer == null) {
+    		$('.answer').append('<p>Your regex returned null</p>');
+    	}else{
+    		$('.answer').append('<p>Your regex returned the answer: ' + answer + '</p>');
+    	}
+    	$('.answer').fadeIn();
+    }
 });
