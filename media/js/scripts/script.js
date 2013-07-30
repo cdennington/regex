9609a93b88c86fb7c3166696f238e169ea449035
Charlie Dennnington <charlie.dennington@u-dox.com>
2013-07-30 10:40:23 +0100

    user can now add extra textareas

--- media/js/scripts/script.js
+++ media/js/scripts/script.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         var regex_function_name = regex.replace(/ /g,"_");
         var search_key = $('.search_key').val();
         var search_sentence = $('.search_sentence').val();
+        console.log(search_sentence);
         var fn = regex_function_name + "('" + search_key + "', '" + search_sentence +"');";
         $('.answer').append('<h3>'+ regex + '</h3>');
         eval(fn);
@@ -14,33 +15,42 @@ $(document).ready(function() {
 
     // Check and calculate regex
     function At_the_beginning_of(search_key, search_sentence) {
-    	var check_regex = search_sentence.match("^" + search_key + "");
-    	$('.answer').append('<p>Add a ^ to the beginning of the string, E.g. "^' + search_key + '"</p>');
-    	answer_regex(check_regex);
+        var check_regex = search_sentence.match("^" + search_key + "");
+        $('.answer').append('<p>Add a ^ to the beginning of the string, E.g. "^' + search_key + '"</p>');
+        answer_regex(check_regex);
     }
     function At_the_end_of(search_key, search_sentence) {
-    	var check_regex = search_sentence.match(search_key + "$");
-    	$('.answer').append('Add a $ to the end of the string, E.g. "' + search_key + '$"');
-    	answer_regex(check_regex);
+        var check_regex = search_sentence.match(search_key + "$");
+        $('.answer').append('Add a $ to the end of the string, E.g. "' + search_key + '$"');
+        answer_regex(check_regex);
     }
     function Exact_match_of(search_key, search_sentence) {
-    	var check_regex = search_sentence.match("^" + search_key + "$");
-    	$('.answer').append('Add a ^ to the beginning of the string and $ to the end, E.g. "^' + search_key + '"$');
-    	answer_regex(check_regex);
+        var check_regex = search_sentence.match("^" + search_key + "$");
+        $('.answer').append('Add a ^ to the beginning of the string and $ to the end, E.g. "^' + search_key + '"$');
+        answer_regex(check_regex);
     }
     function Anywhere(search_key, search_sentence) {
-    	var check_regex = search_sentence.match(search_key);
-    	$('.answer').append('Just put the string in quotes, E.g. "' + search_key + '"');
-    	answer_regex(check_regex);
+        var check_regex = search_sentence.match(search_key);
+        $('.answer').append('Just put the string in quotes, E.g. "' + search_key + '"');
+        answer_regex(check_regex);
     }
 
-    // Tell user the answer of regex
+    // Tell user the answer to regex
     function answer_regex(answer) {
-    	if (answer == null) {
-    		$('.answer').append('<p>Your regex returned null</p>');
-    	}else{
-    		$('.answer').append('<p>Your regex returned true</p>');
-    	}
-    	$('.answer').fadeIn();
+        if (answer == null) {
+            $('.answer').append('<p>Your regex returned null</p>');
+        }else{
+            $('.answer').append('<p>Your regex returned true</p>');
+        }
+        $('.answer').fadeIn();
     }
+
+    // Add extra textareas
+    $(".add_textarea").click(function() {
+        var count_textareas = $('.search_sentence').length;
+        var next_textarea = count_textareas+1;
+        console.log(count_textareas);
+        $('#textarea-'+count_textareas).after('<textarea class="form-control search_sentence" id="textarea-'+ next_textarea +'"></textarea>');
+        return false;
+    });
 });
